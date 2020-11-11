21d5391aee24159416bdc28d9f2e721e346d48e1
Chetan Kini <ckini123@gmail.com>
2020-11-11 14:34:28 -0500

    Start SLL implementation in Python

---
+++ ds/singly-linked-list/py/singly_linked_list.py
@@ -0,0 +1,69 @@
+class Node:
+    def __init__(self, val, next_node):
+        self.val = val
+        self.next = next_node
+
+class SLList:
+    def __init__(self):
+        self.__sentinel = Node(float('inf'), None)
+        self.__prev = self.__sentinel
+        self.__curr = self.__sentinel.next
+        self.__size = 0
+
+    def append(self, item):
+        self.__curr.next = item
+        self.__curr = item
+        self.__size += 1
+
+    def insert(self, item, index):
+        pass # TODO: Open to implement!
+
+    def clear(self):
+        self.__init__()
+
+    def contains(self, item):
+        ptr = self.__sentinel.next
+        while ptr:
+            if ptr.val == item:
+                return True
+            ptr = ptr.next
+        return False
+
+    def get(self, index):
+        if index >= self.__size:
+            return None
+        ptr = self.sentinel.next
+        for _ in range(index):
+            ptr = ptr.next
+        return ptr
+
+    def index_of(self, item):
+        i = 0
+        ptr = self.__sentinel.next
+        while ptr:
+            if ptr == item:
+                return i
+            ptr = ptr.next
+            i += 1
+        return -1
+
+    def is_empty(self):
+        return not self.__size
+
+    def remove_index(self, index):
+        if index >= self.__size:
+
+
+    def remove_item(self, item):
+        pass # TODO: Open to implement!
+
+    def remove_all(self, item):
+        pass # TODO: Open to implement!
+
+    def sort(self):
+        pass # TODO: Open to implement!
+
+    @property
+    def size(self):
+        return self.__size
+
